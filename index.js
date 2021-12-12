b60ca4e3d2f55af67762ef6b217d5728ad3ebfb6
Mike <57681651+mike1234-pixel@users.noreply.github.com>
2021-12-12 13:28:56 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+function decamelize(str, separator){
+	separator = typeof separator === 'undefined' ? ' ' : separator;
+
+	return str
+        .replace(/([a-z\d])([A-Z])/g, '$1' + separator + '$2')
+        .replace(/([A-Z]+)([A-Z][a-z\d]+)/g, '$1' + separator + '$2')
+        .toLowerCase();
+}
+
+const markdownElements = $('.markdown')
+
+markdownElements.each(function() {
+    $(this).html(marked.parse($(this).html()))
+})
+
+$accordionBtn = $('.accordion-button')
+
+$accordionBtn.on('click', function() {
+    let ariaExpanded = $(this).attr('aria-expanded')
+    ariaExpanded === 'false' ? $(this).attr('aria-expanded', 'true'): $(this).attr('aria-expanded', 'false')
+    $(this).next().slideToggle()
+
+    let sectionName = $(this).attr('aria-controls');
+    let figcaption = $(this).next().children('p:first-child').text();
+
+    $('aside').find('img').attr('src', '/assets/' + sectionName + '.jpeg').attr('alt', decamelize(sectionName))
+    $('aside').find('figcaption').text(figcaption)
+})
